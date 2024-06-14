41c43df1840c79d854a4024335f341c32d3d9096
Shintaro Kohtake <skohtake@c2r7s2.42tokyo.jp>
2024-06-14 09:37:34 +0900

    include BUFSIZ

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: skohtake <skohtake@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 12:39:30 by skohtake          #+#    #+#             */
-/*   Updated: 2024/06/13 13:21:07 by skohtake         ###   ########.fr       */
+/*   Updated: 2024/06/14 09:22:04 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
--- tmpdir/tmp_getc.c
+++ tmpdir/tmp_getc.c
@@ -1,8 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tmp_getc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skohtake <skohtake@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 09:22:25 by skohtake          #+#    #+#             */
+/*   Updated: 2024/06/14 09:22:26 by skohtake         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../get_next_line.h"
+
 int	getchar(void)
 {
-	static char buf[BUFSIZ];
-	static char *bufp;
-	static int n = 0;
+	static char	buf[BUFSIZ];
+	static char	*bufp;
+	static int	n = 0;
 
 	if (n == 0)
 	{
