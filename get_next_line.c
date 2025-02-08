1c98a28721723e1a1a7c51dfd7303be5a538699b
Shintaro Kohtake <skohtake@c3r3s1.42tokyo.jp>
2025-02-08 19:17:25 +0900

    add ft_get_char function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: skohtake <skohtake@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 12:34:22 by skohtake          #+#    #+#             */
-/*   Updated: 2025/01/19 13:21:43 by skohtake         ###   ########.fr       */
+/*   Updated: 2025/02/08 19:13:56 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,48 @@ __attribute__((destructor)) static void destructor()
 
 char	*get_next_line(int fd)
 {
-	char *result;
-	char c;
+	char	*result;
+	char	c;
 
 
-	while(1)
+	while (1)
 	{
-		char c = ft_getc(fd);
+		c = ft_getc(fd);
 		if (c == EOF)
-			break;
+			break ;
 		ft_putc(&result, c);
 		if (c == '\n')
-			break;
+			break ;
 	}
 	ft_putc(&result, c);
-	return(result);
+	return (result);
 }
 
+char	ft_get(int fd)
+{
+	static char	buff[BUFFERSIZE];
+	static char	*buffptr;
+	static int	read_byte;
+	char		c;
 
+	if (read_byte == 0)
+	{
+		read_byte = read(fd, buff, BUFFERSIZE);
+		buffptr = buff;
+	}
+
+	read_byte--;
+	if (read_byte >= 0)
+	{
+		c = *buffptr;
+		buffptr++;
+	}
+	if (read_byte < 0)
+	{
+		c = EOF;
+	}
+	return (c);
+}
 
 ////////////
 ////main////
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skohtake <skohtake@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: skohtake <skohtake@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 12:39:30 by skohtake          #+#    #+#             */
-/*   Updated: 2024/06/28 13:48:21 by skohtake         ###   ########.fr       */
+/*   Updated: 2025/02/08 19:07:11 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
 
 //	get_next_line.c
 char	*get_next_line(int fd);
+char    ft_get(int fd);
 
 // get_next_line_utils.c
 
