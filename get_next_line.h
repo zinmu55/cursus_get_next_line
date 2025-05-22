c45e1406bac26a0ea6ea8475302945049c6d7cdb
Shintaro Kohtake <skohtake@c2r8s10.42tokyo.jp>
2025-05-22 13:01:42 +0900

    modify headerfile

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: skohtake <skohtake@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 12:39:30 by skohtake          #+#    #+#             */
-/*   Updated: 2025/05/18 13:48:07 by skohtake         ###   ########.fr       */
+/*   Updated: 2025/05/22 13:00:47 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,49 +18,34 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct s_string
-{
-	char	*str;
-	size_t	len;
-	// size_t	capa;
-}			t_string;
+//	define BUFFER_SIZE
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# if BUFFER_SIZE < 42 && BUFFER_SIZE > 0
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# if BUFFER_SIZE > 2000
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 2000
+# endif
 
 //	get_next_line.c
 
-char		*my_save(char *save);
-char		*my_get_line(char *save);
-char		*my_read(int fd, char *save);
-char		*get_next_line(int fd);
+char	*my_save(char *save);
+char	*my_get_line(char *save);
+char	*my_read(int fd, char *save);
+char	*get_next_line(int fd);
 
 // get_next_line_utils.c
 
-size_t		my_strlen(const char *s);
-char		*my_strchr(const char *s, int c);
-char		*my_initialize(char *s1);
-char		*my_strjoin(char *s1, char *s2);
-
-// // you must comment out below
-
-// # define BUFFER_SIZE 2
-
-// // you must comment out upon
+size_t	my_strlen(const char *s);
+char	*my_strchr(const char *s, int c);
+char	*my_initialize(char *s1);
+char	*my_strjoin(char *s1, char *s2);
 
 #endif
-
-/////////////////////////////////////////////////////////////////////
-
-// #ifndef GET_NEXT_LINE_H
-// # define GET_NEXT_LINE_H
-
-// # include <fcntl.h>
-// # include <stdlib.h>
-// # include <unistd.h>
-// # include <stdio.h>
-
-// //	get_next_line.c
-// char	*get_next_line(int fd);
-// char    ft_get(int fd);
-
-// // get_next_line_utils.c
-
-// #endif
